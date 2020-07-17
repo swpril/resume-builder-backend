c1145a93d58cafe1e0faaf79946e7cc01e2bdf6b
Swpril Ahuja <ahuja.s0007@gmail.com>
2020-07-17 16:51:17 +0530

    add Routes

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const pdf = require('html-pdf');
+const path = require('path');
+
+const app = express();
+
+const port = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.post('/create', (req, res) => {
+    pdf.create(pdfTemplate(req.body), {}).toFile('Resume.pdf', (err) => {
+        if (err)
+            res.send(Promise.reject());
+        res.send(Promise.resolve());
+    })
+});
+
+app.get('/fetch', (req, res) => {
+    res.sendFile(`${__dirname}/Resume.pdf`)
+});
+
+app.listen(port, () => { console.log(`Server is up on PORT ${port}`) });
